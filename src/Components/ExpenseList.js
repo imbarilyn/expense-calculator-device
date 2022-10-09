0c0d9151be23b055492d24880f818640b8e8c129
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-09 08:35:16 +0300

    mapped the fetched data from the server for display

--- src/Components/ExpenseList.js
+++ src/Components/ExpenseList.js
@@ -1,21 +1,28 @@
 import React from 'react'
 
 function ExpenseList() {
+    const expenseItem = expense.map(expe => {
+        //console.log(expe)
+        return(
+           <Expense key = {expe.id} expe ={expe} />
+           
+      )
+    }
+    )
   return (
     <div className='mt-3'>
     <h2>Expense Summary</h2>
     <table className="table">
-<thead className="thead-dark">
-  <tr>
-    
-    <th scope="col">Description</th>
-    <th scope="col">Cost</th>
-  </tr>
-</thead>
-<tbody>    
-  {expenseItem}  
-</tbody> 
-</table>
+        <thead className="thead-dark">
+        <tr>
+            <th scope="col">Description</th>
+            <th scope="col">Cost</th>
+        </tr>
+        </thead>
+        <tbody>    
+        {expenseItem}  
+        </tbody> 
+    </table>
 </div>
    
   )
