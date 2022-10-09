40feaaca51d9ac8b195761abdd9c3122268f9c67
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-09 08:45:32 +0300

    created table tada fields for displaying data

---
+++ src/Components/Expense.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Expense() {
+  return (
+    <tr>
+    {/* <th scope="row">{key}</th> */}
+    <td>{description}</td>
+    <td>{cost}</td>          
+  </tr>  
+    
+  )
+}
+
+export default Expense
