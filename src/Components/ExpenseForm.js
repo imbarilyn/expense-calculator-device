8b8b8e8fb9d28b61373d1fb8e5fca8b747be62ee
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-09 08:08:30 +0300

    Created a form

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -2,7 +2,28 @@ import React, { useState } from 'react'
 
 function ExpenseForm() {
   return (
-    <div>ExpenseForm</div>
+    <div>
+             <form onSubmit={submitHandler} className ="container">
+          <div className="form-group row mt-3">
+            <label htmlFor="description" className="col-sm-2 col-form-label">Description</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control col-6" id="description" placeholder="Enter expense" name = "description" value ={description}  onChange = {desHandler} />
+              {/* <p>{errors.description?.message}</p> */}
+            </div>
+          </div>  
+          <div className="form-group row mt-3">
+            <label htmlFor="cost" className="col-sm-2 col-form-label">Cost</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="cost" placeholder="Enter amount" name ="cost" value ={cost}  onChange = {costHandler} />
+              {/* <p>{errors.cost?.message}</p> */}
+            </div>
+            <div className='col-sm mt-3 mr-4' >
+            <button type="submit" className="btn btn-primary">Add Expense</button>
+          </div>
+        </div>
+      </form>
+ 
+    </div>
   )
 }
 
