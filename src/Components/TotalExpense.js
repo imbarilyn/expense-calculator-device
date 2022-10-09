c6e4d97a1cadc1d3eb609cd13af92d29247b6c4f
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-09 09:02:15 +0300

    created the body of te modal

--- src/Components/TotalExpense.js
+++ src/Components/TotalExpense.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 function TotalExpense() {
   return (
+    <div className='modal'>
+    {/* {alert("Hello")} */}
+    <div className='modal-container'>
+      <div className='modal-title'>
+        <h2>Hey there. Your expense is:</h2>
+      </div>
+      <div className='modal-body'>
+        <p>{totals}</p>
+      </div>
+      <div className='modal-footer'>
+        <button className = " btn btn-danger" onClick = {() => setCloseModal(false)}>close</button>
+      </div>
+    </div>
+    
+  </div>
     
   )
 }
