6e695559a9c7834ee8fa3f7643e2046d22ece0ab
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-09 08:31:27 +0300

    Created the data table

--- src/Components/ExpenseList.js
+++ src/Components/ExpenseList.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function ExpenseList() {
   return (
-    <div></div>
+    <div className='mt-3'>
+    <h2>Expense Summary</h2>
+    <table className="table">
+<thead className="thead-dark">
+  <tr>
+    
+    <th scope="col">Description</th>
+    <th scope="col">Cost</th>
+  </tr>
+</thead>
+<tbody>    
+  {expenseItem}  
+</tbody> 
+</table>
+</div>
+   
   )
 }
 
