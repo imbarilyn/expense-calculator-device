91df9709d123d12767f98f2f312466d41ea3b021
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-10-09 08:42:08 +0300

    mapped the data from server

--- src/Components/Expense.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Expense() {
-  return (
-    <div>Expense</div>
-  )
-}
-
-export default Expense
--- src/Components/ExpenseList.js
+++ src/Components/ExpenseList.js
@@ -1,31 +1,32 @@
 import React from 'react'
 
-function ExpenseList() {
-    const expenseItem = expense.map(expe => {
-        //console.log(expe)
-        return(
-           <Expense key = {expe.id} expe ={expe} />
-           
-      )
-    }
-    )
+function Expense({expense}) {
+  const expenseItem = expense.map(expe => {
+    //console.log(expe)
+    return(
+       <Expense key = {expe.id} expe ={expe} />
+       
+  )
+}
+)
   return (
     <div className='mt-3'>
-    <h2>Expense Summary</h2>
-    <table className="table">
-        <thead className="thead-dark">
-        <tr>
-            <th scope="col">Description</th>
-            <th scope="col">Cost</th>
-        </tr>
-        </thead>
-        <tbody>    
-        {expenseItem}  
-        </tbody> 
-    </table>
-</div>
-   
+      <h2>Expense Summary</h2>
+      <table className="table">
+  <thead className="thead-dark">
+    <tr>
+      
+      <th scope="col">Description</th>
+      <th scope="col">Cost</th>
+    </tr>
+  </thead>
+  <tbody>    
+    {expenseItem}  
+  </tbody> 
+</table>
+    </div>
+    
   )
 }
 
-export default ExpenseList
+export default Expense
